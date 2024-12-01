502557e0555337ee8851e5d28a0a54b17bafb498
hassaammgl <h455a4mmehtab@gmail.com>
2024-12-01 22:34:08 +0500

    folder structure setup Completed

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const ibmPlexSans = IBM_Plex_Sans({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-ibm-plex",
+});
+
+export const metadata: Metadata = {
+	title: "Imaginigan",
+	description: "Ai-Powered kekegenkai image editor.",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body
+				className={cn(
+					"font-ibm-plex antialiased",
+					ibmPlexSans.variable
+				)}
+			>
+				{children}
+			</body>
+		</html>
+	);
+}
