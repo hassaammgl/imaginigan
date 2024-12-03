9c7deacd7575dcf150e6343d44ebd7543db4b365
hassaammgl <h455a4mmehtab@gmail.com>
2024-12-03 10:34:37 +0500

    starting up zod

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
 import React from "react";
+import { useUser } from "@clerk/nextjs";
 
 const Profile = () => {
-	return <div>Profile</div>;
+	const { user } = useUser();
+
+	if (!user) {
+		return <div>Loading...</div>;
+	}
+
+	return (
+		<div>
+			{/* // Profile: // <div>User: {user.firstName}</div>
+			// <div>Full Name: {user.fullName}</div>
+			// <div>Email: {user.emailAddresses[0].emailAddress}</div>
+			// <div>Email: {user.id}</div>
+			// <div>Email: {user.imageUrl}</div>
+			// <img src={user.imageUrl} alt="" /> */}
+		</div>
+	);
 };
 
 export default Profile;
