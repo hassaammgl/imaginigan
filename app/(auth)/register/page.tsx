81a9b72816f253a0990e952ebce42413fe8b9574
hassaammgl <h455a4mmehtab@gmail.com>
2024-12-04 10:52:23 +0500

    setting up register route for mongodb

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import React from "react";
+
+const page = () => {
+	return <div>page</div>;
+};
+
+export default page;
